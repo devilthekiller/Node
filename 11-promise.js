4376ca557744979208365c47f5281f27566eac99
Ayush Gupta <117255251+devilthekiller@users.noreply.github.com>
2024-02-11 02:23:45 +0530

    nk

--- 11-promise.js
+++ 11-promise.js
@@ -4,9 +4,7 @@ const { readFile, writeFile } = require('fs')
 const util  = require('util')
 const readFilePromise = util.promisify(readFile)
 const writeFilePromise = util.promisify(writeFile)
-
-
-
+    
 const start = async () => {
     try {
         const first = await readFilePromise("./2-sayhi.js", 'utf-8')
--- 9-settimout.js
+++ 9-settimout.js
@@ -10,6 +10,6 @@ console.log("6");
 console.log("7");
 var count=1;
 setInterval(()=>{
-        count*=2;
+        count*=2;    
     console.log(count);
 },10)
